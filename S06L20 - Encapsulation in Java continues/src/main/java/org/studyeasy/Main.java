372b42f59ea4891d71ad132bd0e72885590797e5
sravani-cmyk <sravanikandukuri76@gmail.com>
2024-09-16 23:41:11 +0530

    Add files via upload

---
+++ S06L20 - Encapsulation in Java continues/src/main/java/org/studyeasy/Main.java
@@ -0,0 +1,12 @@
+package org.studyeasy;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("John", 25, "Male");
+//        person.setName("Mike");
+//        person.setAge(200);
+        person.setName("sravani");
+        System.out.println(person);
+
+    }
+}
---
+++ S06L21 - Polymorphism in Java/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.studyeasy.Nokia3310;
+import org.studyeasy.Phone;
+
+public class Main {
+    public static void main(String[] args) {
+//        Phone phone = new phone();
+        Phone phone1 = new Nokia3310();
+        phone1.feature();
+
+    }
+}
